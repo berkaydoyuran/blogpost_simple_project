2f2bc0d9d47f6e668e8a735e174f06e7145bf483
Berkay Doyuran <berkay.doyuran02@gmail.com>
2023-08-09 19:43:43 +0300

    Ufak mesaj düzenlemeleri

--- post/models.py
+++ post/models.py
@@ -15,7 +15,6 @@ class Post(models.Model):
     is_deleted = models.BooleanField(default=False)
 
     def deleteds(self):
-        """Mark the record as deleted instead of deleting it""" 
 
         self.is_deleted = True
         self.save()
